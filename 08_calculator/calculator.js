776fa52f90e6cc98d717325b83c72fa2eb9fe434
C.N <cn.mbhalati@gmail.com>
2022-03-04 19:45:59 +0200

    Complete Calculator Task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const answer = array.reduce((total, item) => {
+    return total += item
+  }, 0);
+  return answer
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const answer =  array.reduce((total, item) => {
+      return total *= item;
+  }, 1);
+  return answer
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let answer = 1
+  if (a == 1 || a == 0) {
+    return 1
+  } 
+  for (i = 2; i <= a; i++) {
+    answer = answer * i
+  }
+  return answer
 };
 
 // Do not edit below this line
