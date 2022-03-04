699fb6230dbe4588c66da98f9baf26e9342dde56
C.N <cn.mbhalati@gmail.com>
2022-03-04 22:38:16 +0200

    Complete and optimize Palindrome code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const re = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g
 
+const palindromes = function (string) {
+    return joinString(string) === reverseString(string)
 };
+
+const reverseString = (string) => string
+                                .replaceAll(re, '')
+                                .replaceAll(' ','')
+                                .toLowerCase()
+                                .split('')
+                                .reverse()
+                                .join('')                
+
+const joinString = (string) => string
+                                .replaceAll(re, '')
+                                .replaceAll(' ','')
+                                .toLowerCase()
 
 // Do not edit below this line
 module.exports = palindromes;
