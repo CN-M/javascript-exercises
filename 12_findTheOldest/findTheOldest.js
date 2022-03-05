86f8cff92e9e59d03b6a6df6e156609bc0a7efdf
C.N <cn.mbhalati@gmail.com>
2022-03-05 11:06:27 +0200

    Complete findOldest challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.sort((a,b) => {
+        if (!b.yearOfDeath)
+            b.yearOfDeath = (new Date().getFullYear())
+        
+        let person_A = a.yearOfDeath - a.yearOfBirth;
+        let person_B = b.yearOfDeath - b.yearOfBirth;
+        if (person_A > person_B) {
+            return -1
+        } 
+        return 1
+    });
+    return array[0]
 };
 
 // Do not edit below this line
