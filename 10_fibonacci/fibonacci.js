5ad3b21c875d18ffd58eeac00d77ecc575602614
C.N <cn.mbhalati@gmail.com>
2022-03-05 09:43:34 +0200

    Complete Fibonacci problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0)
+        return 'OOPS'
+    if (index == 1 || index == 2)
+        return 1
+    seq = [];
+        for (let i = 0; i < index; i++) {
+            if (seq.length < 3) {
+                seq.push(i)
+            } else {
+                seq[i] = seq[i-1] + seq[i-2]
+            }
+        }
+    return seq[index-1]
 };
+
+console.table(fibonacci(4))
 
 // Do not edit below this line
 module.exports = fibonacci;
